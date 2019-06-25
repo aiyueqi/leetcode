5913c4b1cd1f7eea529cc09bb3e43f90a25d06cf
aiyueqi <yueqi.ai@ximalaya.com>
2019-06-25 23:34:52 +0800

    add a206

--- python/a206.py
+++ python/a206.py
@@ -7,6 +7,7 @@ from typing import List, Dict, Tuple
 #         self.next = None
 
 class Solution:
+
 #     def f(self, p, q):
 #         if p == q: return p
 #         tmp = p.next
@@ -22,6 +23,18 @@ class Solution:
 #             tail = tail.next
 #         return self.f(head, tail)
 
+#迭代法
+	# def reverseList(self, head: ListNode) -> ListNode:
+ #        p = None
+ #        q = head
+ #        while(q):
+ #            tmp = q.next
+ #            q.next = p
+ #            p = q
+ #            q = tmp
+ #        return p
+
+#递归解法
     def f(self, p, q):
         if q == None: return p
         tmp = q.next
